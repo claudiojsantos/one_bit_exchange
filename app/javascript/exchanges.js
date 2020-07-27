88e9ac616e96ccd7040a948cdeb0ffbf997480fe
Cláudio Santos <cljos@hotmail.com>
2020-07-26 21:07:42 -0300

    desafio 02 concluido

--- app/javascript/exchanges.js
+++ app/javascript/exchanges.js
@@ -3,4 +3,33 @@ document.addEventListener('turbolinks:load', function(){
         let [result] = event.detail
         document.getElementById('result').value = result.value
     })
+
+    document.getElementById('btnInverterUp').addEventListener('click', function(){
+        firstsel = document.getElementById('source_currency').value
+        secondsel = document.getElementById('target_currency').value
+        
+        document.getElementById('source_currency').value = secondsel
+        document.getElementById('target_currency').value = firstsel
+    })
+
+    $("#amount").keyup(function(){
+        let source_currency = $('#source_currency').val()
+        let target_currency = $('#target_currency').val()
+        let amount = this.value
+        
+        let url = 'http://localhost:3000/convert'
+        
+        $.ajax({
+            method: "GET",
+            url: url,
+            data: {
+                source_currency: source_currency,
+                target_currency : target_currency,
+                amount: amount
+            },
+            success: function(data) {
+                $('#result').val(JSON.stringify(data.value))
+            }
+        })
+    })
 })
